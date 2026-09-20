0a4b575c2d4d3cde336145dad2649ea0638a05f1
omagbogan <omagbogan@gmail.com>
2026-09-20 20:09:17 +0000

    Carte responsive

--- nofna-website/components/InteractiveMap.tsx
+++ nofna-website/components/InteractiveMap.tsx
@@ -132,7 +132,7 @@ export default function InteractiveMap({ projects }: { projects: any[] }) {
     </div>
 
     {/* Liste des projets */}
-    <div className="bg-white rounded-lg overflow-hidden shadow-sm overflow-y-auto max-h-64 md:max-h-full p-2">
+    <div className="bg-white rounded-lg overflow-hidden shadow-sm overflow-y-auto max-h-96 md:max-h-full p-2">
       <div className="space-y-2">
         {withCoords.map((p) => (
           <button
